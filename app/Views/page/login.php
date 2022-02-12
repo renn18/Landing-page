53748e9c404454a927be1a9f1770818f50e177cb
renn18 <ppskdos>
2022-02-12 11:06:36 +0800

    mengimplementasikan template page layout pada login

--- app/Views/page/login.php
+++ app/Views/page/login.php
@@ -1,18 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<title>Login Page</title>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">	
-	<link rel="icon" type="image/png" href="<?php echo base_url('/assets/image/favicon.png')?>"/>
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url('/assets/vendor/bootstrap/css/bootstrap.min.css')?>">
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url('/assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css')?>">
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url('/assets/vendor/animate/animate.css')?>">	
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url('/assets/vendor/css-hamburgers/hamburgers.min.css')?>">
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url('/assets/vendor/select2/select2.min.css')?>">
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url('/assets/css/util-login.css')?>">
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url('/assets/css/main-login.css')?>">
-</head>
+<?= $this->extend('template/page-layout') ?>
+
+<?= $this->section('content') ?>
+
 <body>
 	
 	<div class="limiter">
@@ -68,20 +57,5 @@
 			</div>
 		</div>
 	</div>
-	
-	
-	
-	<script src="<?php echo base_url('/assets/vendor/jquery/jquery-3.2.1.min.js')?>"></script>
-	<script src="<?php echo base_url('/assets/vendor/bootstrap/js/popper.js')?>"></script>
-	<script src="<?php echo base_url('/assets/vendor/bootstrap/js/bootstrap.min.js')?>"></script>
-	<script src="<?php echo base_url('/assets/vendor/select2/select2.min.js')?>"></script>
-	<script src="<?php echo base_url('/assets/vendor/tilt/tilt.jquery.min.js')?>"></script>
-	<script >
-		$('.js-tilt').tilt({
-			scale: 1.1
-		})
-	</script>
-	<script src="<?php echo base_url('/assets/js/main.js')?>"></script>
 
-</body>
-</html>
+<?= $this->endSection() ?>
