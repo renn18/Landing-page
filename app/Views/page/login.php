96015a8f6ed7e003732b4255cdc0f375126eca17
renn18 <ppskdos>
2022-02-12 08:31:12 +0800

    mengubah link pada login menjadi base_url

--- app/Views/page/login.php
+++ app/Views/page/login.php
@@ -6,12 +6,12 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">	
 	<link rel="icon" type="image/png" href="<?php echo base_url('/assets/image/favicon.png')?>"/>
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('/assets/vendor/bootstrap/css/bootstrap.min.css')?>">
-	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">	
-	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
-	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
-	<link rel="stylesheet" type="text/css" href="css/util.css">
-	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('/assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css')?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('/assets/vendor/animate/animate.css')?>">	
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('/assets/vendor/css-hamburgers/hamburgers.min.css')?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('/assets/vendor/select2/select2.min.css')?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('/assets/css/util-login.css')?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('/assets/css/main-login.css')?>">
 </head>
 <body>
 	
@@ -19,7 +19,7 @@
 		<div class="container-login100">
 			<div class="wrap-login100">
 				<div class="login100-pic js-tilt" data-tilt>
-					<img src="images/img-01.png" alt="IMG">
+					<img src="<?php echo base_url('/assets/image/img-01.png')?>" alt="IMG">
 				</div>
 
 				<form class="login100-form validate-form">
@@ -27,7 +27,7 @@
 						Member Login
 					</span>
 
-					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@gmail.com">
 						<input class="input100" type="text" name="email" placeholder="Email">
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
@@ -70,24 +70,18 @@
 	</div>
 	
 	
-
 	
-<!--===============================================================================================-->	
-	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
-<!--===============================================================================================-->
-	<script src="vendor/bootstrap/js/popper.js"></script>
-	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-<!--===============================================================================================-->
-	<script src="vendor/select2/select2.min.js"></script>
-<!--===============================================================================================-->
-	<script src="vendor/tilt/tilt.jquery.min.js"></script>
+	<script src="<?php echo base_url('/assets/vendor/jquery/jquery-3.2.1.min.js')?>"></script>
+	<script src="<?php echo base_url('/assets/vendor/bootstrap/js/popper.js')?>"></script>
+	<script src="<?php echo base_url('/assets/vendor/bootstrap/js/bootstrap.min.js')?>"></script>
+	<script src="<?php echo base_url('/assets/vendor/select2/select2.min.js')?>"></script>
+	<script src="<?php echo base_url('/assets/vendor/tilt/tilt.jquery.min.js')?>"></script>
 	<script >
 		$('.js-tilt').tilt({
 			scale: 1.1
 		})
 	</script>
-<!--===============================================================================================-->
-	<script src="js/main.js"></script>
+	<script src="<?php echo base_url('/assets/js/main.js')?>"></script>
 
 </body>
 </html>
