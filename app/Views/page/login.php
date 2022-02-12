d9c070e9320d11833394a10c28c3f9823326767d
renn18 <ppskdos>
2022-02-12 12:47:00 +0800

    mengubah design login page

--- app/Views/page/login.php
+++ app/Views/page/login.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+	<head>
 	<title>Login Page</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">	
@@ -13,6 +13,41 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('/assets/css/util-login.css')?>">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('/assets/css/main-login.css')?>">
 </head>	
+<body>
+	<div class="container">
+		<nav class="navbar navbar-expand-lg navbar-light">
+			<a class="navbar-brand" href="/">Landing Page</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item active">
+					<a class="nav-link" href="/" target="_blank">Home <span class="sr-only">(current)</span></a>
+				</li>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						Menu
+					</a>
+					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+						<a class="dropdown-item" href="#">Landing Page</a>
+						<a class="dropdown-item active" href="#">Login/Signup</a>
+						<a class="dropdown-item" href="#">Dashboard</a>
+						<a class="dropdown-item" href="#">Contact Us</a>
+					</div>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">About</a>
+				</li>
+			</ul>
+			<form class="form-inline my-2 my-lg-0">
+				<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+				<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+			</form>
+		</div>
+	</nav>
+</div>
 	<div class="limiter">
 		<div class="container-login100">
 			<div class="wrap-login100">
@@ -56,7 +91,7 @@
 						</a>
 					</div>
 
-					<div class="text-center p-t-136">
+					<div class="text-center p-t-50">
 						<a class="txt2" href="#">
 							Create your Account
 							<i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
